c25274e8b5e6e2f2e6260c0c0a9355a271540627
Lksntzz <lucasmanga95@gmail.com>
2026-08-06 20:39:43 -0300

    replace history guard with focused income shortcuts

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ import './lib/guided-tutorial-mount';
 import './lib/monthly-fixed-bills-mount';
 import './lib/standalone-insights-mount';
 import './lib/unified-transaction-launcher-mount';
-import './lib/history-income-balance-guard';
+import './lib/income-shortcuts-mount';
 import './lib/release-update-notification-mount';
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
