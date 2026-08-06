d83d0293e9391a3bc4c455f19f865371c13656e5
Lksntzz <lucasmanga95@gmail.com>
2026-08-06 15:16:49 -0300

    load release update notice in notification center

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,7 @@ import './lib/monthly-fixed-bills-mount';
 import './lib/standalone-insights-mount';
 import './lib/insights-fixed-bill-count-guard';
 import './lib/unified-transaction-launcher-mount';
+import './lib/release-update-notification-mount';
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
