e952198f7866623350f8a646c9696dd0c14668f9
Lksntzz <lucasmanga95@gmail.com>
2026-08-10 00:39:04 -0300

    feat: route iPhone quick actions into MF mobile

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -7,43 +7,61 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        // Sent when the application is about to move from active to inactive state.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        // Release shared resources here if native-only state is introduced later.
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        // Capacitor/App handles the corresponding foreground event for the web core.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        // The web core refreshes time-sensitive data when its screens load.
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // No native-only financial state is persisted here.
     }
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        // Called when the app was launched with a url. Feel free to add additional processing here,
-        // but if you want the App API to support tracking app url opens, make sure to keep this call
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        // Called when the app was launched with an activity, including Universal Links.
-        // Feel free to add additional processing here, but if you want the App API to support
-        // tracking app url opens, make sure to keep this call
         return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 
+    func application(
+        _ application: UIApplication,
+        performActionFor shortcutItem: UIApplicationShortcutItem,
+        completionHandler: @escaping (Bool) -> Void
+    ) {
+        guard let url = urlForShortcut(shortcutItem.type) else {
+            completionHandler(false)
+            return
+        }
+
+        let handled = ApplicationDelegateProxy.shared.application(application, open: url, options: [:])
+        completionHandler(handled)
+    }
+
+    private func urlForShortcut(_ type: String) -> URL? {
+        switch type {
+        case "br.com.mfinanceiro.app.quick":
+            return URL(string: "mfinanceiro://quick")
+        case "br.com.mfinanceiro.app.scan":
+            return URL(string: "mfinanceiro://scan")
+        case "br.com.mfinanceiro.app.pulse":
+            return URL(string: "mfinanceiro://pulse")
+        default:
+            return nil
+        }
+    }
 }
