85e587f1f5d0e99098270acdb77f88a247a9c028
Lksntzz <lucasmanga95@gmail.com>
2026-08-10 00:35:45 -0300

    feat: capture Android share intents

--- android/app/src/main/java/br/com/mfinanceiro/app/MainActivity.java
+++ android/app/src/main/java/br/com/mfinanceiro/app/MainActivity.java
@@ -1,5 +1,22 @@
 package br.com.mfinanceiro.app;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(NativeShareReceiverPlugin.class);
+        NativeShareStore.capture(this, getIntent());
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        NativeShareStore.capture(this, intent);
+    }
+}
