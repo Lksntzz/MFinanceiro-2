7fc5f703a110654939776efda5ffae80d2d70c85
Lksntzz <lucasmanga95@gmail.com>
2026-08-11 00:48:25 -0300

    fix: retire global transaction launcher interceptor

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 // Bank format adapters are pure parsing compatibility layers.
-import './lib/unified-transaction-launcher-mount';
 import './lib/mega-update-announcement-mount';
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
